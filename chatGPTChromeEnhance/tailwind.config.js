759e2c830d52c7ef1198f81359ea361a90698391
purelovers <113244554+purelovers@users.noreply.github.com>
2022-12-24 13:02:04 +0000

    Remove --stderr switch from Travis test runner

---
+++ chatGPTChromeEnhance/tailwind.config.js
@@ -0,0 +1,16 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  corePlugins: {
+    preflight: false,
+  },
+  content: ["./src/**/*.{html,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["valentine"],
+  },
+  prefix: "wcg-",
+}
